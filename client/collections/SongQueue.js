828a396a892a7fb3cca348962e16719a2c9628bb
calholl <calvin.c.holloway@gmail.com>
2016-11-14 21:04:11 -0800

    completed queue prompts

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,37 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+    this.on('add', function(song) {
+      // need to implement some condition to run the playFirst
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function() {
+      // console.log('before the shift: ' + JSON.stringify(this, null, 2));
+      this.shift();
+      // console.log('after the shift: ' + JSON.stringify(this, null, 2));
+      // if more exist in list, play next in line
+      if (this.length >= 1) {
+        this.playFirst();
+      }
+    });
+
+    //Added a remove event to listen for:
+    // this.on('remove', function(song) {
+    //   this.remove(song);
+    // });
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  },
+
+  // causes infinite loop
+  remove: function(song) {
+    console.log(JSON.stringify(song));
+    // this.(song);
+  },
 
 });
--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -4,6 +4,42 @@ var SongModel = Backbone.Model.extend({
   play: function() {
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+
+  enqueue: function() {
+    // Triggering an event here will also trigger the event on the collection
+    this.trigger('enqueue', this);
+  },
+
+  dequeue: function() {
+    // Triggering an event here will also trigger the event on the collection
+    this.trigger('dequeue', this);
+  },
+
+  ended: function() {
+    // Triggering an event here will also trigger the event on the collection
+    this.trigger('ended', this);
   }
+
+////////////////////////////  Song Queuing functionality
+
+
+
+  // add: function(songData) {
+  //   this.songQueue.push(songData);
+  //   if (this.songQueue.length === 1) {
+  //     this.trigger('playFirst', this);
+  //   }
+  // }
+
+  // playFirst: function() {
+  //   //play index[0] in songQueue
+  //   this.play(this.songQueue[0]);   // THIS NEEDS REWORK
+  //   //when finished remove index[0]
+  //   this.dequeue();
+  //
+  // }
+
+
 
 });
